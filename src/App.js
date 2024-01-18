8732fc34fca589c3554c6164c44329ed41c5a79d
16-Doffy <ngophamnhutduy050302@gmail.com>
2024-01-18 20:52:19 +0700

    Body2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+
 import React from 'react';
+import './App.css';
+
+import Body2 from './components/body/Body2';
+
 
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div className="App">
+    
+      <Body2 />
+    </div>
+  );
 }
 
 export default App;
+
---
+++ src/components/body/Body2.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Body2 = () => {
+  return (
+    <body className="Body2">
+      <div className="Body2-row">
+        <div className="Body2-column">
+        <span className="icon-container ">
+          <img src="https://noithatantin.com/wp-content/uploads/2023/01/icon-thiet-ke-noi-that-400x400.png" alt="Icon" />
+          </span>
+          <h1> Thiết kế nội thất miễn phí</h1>
+          <div className="Body2">
+            <p> <strong>Bạn muốn xây nhà mới </strong> căn bảng về nội thất chi tiết từng không gian?</p> 
+            <div className="boostest">TRAO ĐỔI CÙNG KST</div>
+          </div>
+        </div>
+        <div className="Body2-column">
+          <span className="icon-container ">
+          <img src="https://noithatantin.com/wp-content/uploads/2023/01/icon-thiet-ke-noi-that-400x400.png" alt="Icon" />
+          </span>
+          <h1>Tìm đơn vị thi công uy tín</h1>
+        
+          <p><strong>Bạn muốn cải tạo nhà</strong> cần tìm đội ngũ tay nghề cao cam kết tiến độ và chất lượng</p>
+          <div className="boostest">NHẬN BÁO GIÁ NGAY</div>
+        </div>
+        <div className="Body2-column">
+        <span className="icon-container ">
+          <img src="https://banner2.cleanpng.com/20180613/bux/kisspng-computer-icons-counseling-psychology-business-cons-globalization-5b215a402ee8d6.8483161115289124481922.jpg" alt="Icon" />
+          </span>
+          <h1>Tư vấn 1:1 cùng KTS</h1>
+          <p><strong>Bạn đang lên ý tưởng</strong> cần định hình phong cách và nội thất phù hợp</p>
+          <div className="boostest">ĐĂNG KÍ TƯ VẤN</div>
+        </div>
+        
+          {/* <div className="icon">
+            <FontAwesomeIcon icon={faYoutube} />
+            <FontAwesomeIcon icon={faFacebook} />
+            <FontAwesomeIcon icon={faFacebookMessenger} />
+          </div> */}
+        
+      </div>
+    </body>
+  );
+};
+
+export default Body2;
