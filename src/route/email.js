e48813098e17c67c75fb069d3158d155331de0cd
TanHuynh68 <tanhpce171112@fpt.edu.vn>
2024-03-27 12:59:48 +0700

    complete

--- src/route/email.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const emailController = require('../controllers/emailController');
-const { authenToken, authenToken2, isUser } = require('../middleware/JWT');
-const upload = require('../middleware/upload');
-router.post('/sendEmail', emailController.sendEmail)
-module.exports = router
